106dcd71ffe87ef5283f76fcf0cdb3606d5c05dc
Alexandre Ngau <alexandrengau@hotmail.fr>
2024-01-03 17:13:04 +0100

    commenting the code

--- wikipedia_michelin_stars/wikipedia_michelin_stars/spiders/wikipedia_spider.py
+++ wikipedia_michelin_stars/wikipedia_michelin_stars/spiders/wikipedia_spider.py
@@ -8,25 +8,31 @@ class WikipediaSpider(scrapy.Spider):
     name = "wikipedia"
 
     def start_requests(self):
+        # Starting URL for the Spider
         url = "https://fr.wikipedia.org/wiki/Liste_des_restaurants_deux_et_trois_étoiles_du_Guide_Michelin"
+        # Make a request to the URL, and when the response is received, call the parse method
         yield scrapy.Request(url=url, callback=self.parse)
 
     # Method to handle the response from each request
     def parse(self, response):
+        # Save the HTML content of the response to a file for inspection
         filename = f"./two_three_stars_restaurant.html"
         Path(filename).write_bytes(response.body)
         self.log(f"Saved file {filename}")
 
-        # List of regions to exclude
+        # List of regions to exclude (will be used throughout the parsing)
         excluded_regions = [
             "Allemagne", "Italie", "Espagne", "Royaume-Uni",
             "Pays-Bas", "Belgique", "Suisse", "Autriche",
             "Danemark", "Norvège", "Suède"
         ]
 
+        ### THREE STAR RESTAURANTS ###
+
+        # List to store 3-star restaurants
         restaurants = []
 
-        # Assuming the relevant data is in a table, adjust the selector accordingly
+        # Assuming the relevant data is in a table
         rows = response.css('table.wikitable tbody tr')
 
         for row in rows:
@@ -36,6 +42,7 @@ class WikipediaSpider(scrapy.Spider):
             # Concatenate name and name_it, handling null values
             restaurant_name = name + (' ' + name_i if name_i else '') if name else name_i
 
+            # Dictionary to store restaurant details
             restaurant = {
                 'restaurant-name': restaurant_name,
                 'restaurant-region': row.css('td:nth-child(6) a::text').get(),
@@ -48,8 +55,13 @@ class WikipediaSpider(scrapy.Spider):
             if restaurant['restaurant-city'] is not None and restaurant['restaurant-region'] not in excluded_regions:
                 restaurants.append(restaurant)
 
+            # Break the loop after collecting 29 restaurants
+            # ==> Wikipedia says there are 29 3-star restaurants in France and Monaco
+            # at the date the website was looked up (January 3rd, 2024)
             if len(restaurants) >= 29:
                 break
+
+        ### TWO STAR RESTAURANTS ###
 
         # Find the tables after the specified text
         table_selector = response.xpath('//p[i[contains(text(), "Liste des 75 restaurants deux étoiles du Guide '
@@ -58,14 +70,17 @@ class WikipediaSpider(scrapy.Spider):
         # Extract the table HTML
         table_html = table_selector.extract_first()
 
+        # List to store 2-star restaurants
         restaurants_2 = []
 
+        # Create a new scrapy Selector from the extracted HTML
         rows = scrapy.Selector(text=table_html).css('tbody tr')
 
         for row in rows:
             name = row.css('td:nth-child(2) i::text').get()
             name_i = row.css('td:nth-child(2) i a::text').get()
             name_td = row.css('td:nth-child(2)::text').get()
+
             # Concatenate name and name_i, handling null values
             restaurant_name = ' '.join(filter(None, [name, name_i]))
             # If restaurant_name is still empty, use name_td
@@ -76,6 +91,7 @@ class WikipediaSpider(scrapy.Spider):
             # Concatenate chef and name_i, handling null values
             chef_name = chef + (' ' + chef_td if chef_td else '') if chef else chef_td
 
+            # Dictionary to store restaurant details
             restaurant_2 = {
                 'restaurant-name': restaurant_name,
                 'restaurant-region': row.css('td:nth-child(5) a::text').get(),
@@ -88,7 +104,11 @@ class WikipediaSpider(scrapy.Spider):
             if restaurant_2['restaurant-city'] is not None and restaurant_2['restaurant-region'] not in excluded_regions:
                 restaurants_2.append(restaurant_2)
 
+            # Break the loop after collecting 75 restaurants
+            # ==> Wikipedia says there are 75 2-star restaurants in France and Monaco
+            # at the date the website was looked up (January 3rd, 2024)
             if len(restaurants_2) >= 75:
                 break
 
-        yield {'restaurants': restaurants+restaurants_2}
+        # Yield the final result as a dictionary with a list of restaurants
+        yield {'restaurants': restaurants + restaurants_2}
